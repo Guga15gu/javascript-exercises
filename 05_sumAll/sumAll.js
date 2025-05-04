933891bf9b7406bf64447a6735817d339b36424a
Gustavo Machado <48689692+Guga15gu@users.noreply.github.com>
2025-05-04 16:13:54 -0300

    complete 05, 06 and 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let sum = 0;
 
+    if (!(Number.isInteger(x) && Number.isInteger(y))){
+        return 'ERROR';
+    }
+    if ((x < 0) || (y < 0)){
+        return 'ERROR';
+    } 
+
+    let x1 = x;
+    let y1 = y;
+    
+    if (x > y){
+        x1 = y;
+        y1 = x;
+    }
+
+    for(; x1 <= y1; ++x1){
+        sum += x1;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
